d6728dbf5567d8bc39b60b0704902de79252d660
Victormuthomi <victormuhomi100@gmail.com>
2024-09-24 22:55:06 +0300

    Add the views for the workers app.

--- workers/views.py
+++ workers/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+
+from .models import Worker 
+
 # Create your views here.
+
+class Workerlistview(ListView):
+    model = Worker
+
