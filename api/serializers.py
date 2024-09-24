b31c9cb6370f0ec0b9aa1636a74b17f21ecbd55a
Victormuthomi <victormuhomi100@gmail.com>
2024-09-24 22:51:15 +0300

    Add the serializer for the workers app.

--- api/serializers.py
+++ api/serializers.py
@@ -2,8 +2,16 @@ from rest_framework import serializers
 
 from books.models import Book 
 
+from workers.models import Worker
+
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('title' ,'subtitle', 'author', 'isbn')
+
+class WorkerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Worker
+        fields = ('name', 'id_number', 'member_number')
+
     
