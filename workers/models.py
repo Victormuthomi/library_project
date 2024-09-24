cfe5bee8251b9606c4a82a517084ade42b8acc38
Victormuthomi <victormuhomi100@gmail.com>
2024-09-24 22:54:39 +0300

    Add the Model Worker.

--- workers/models.py
+++ workers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Worker(models.Model):
+    """The model for the workers"""
+    name = models.CharField(max_length=25)
+    id_number = models.IntegerField()
+    member_number = models.IntegerField()
+
+    def __str__(self):
+        return self.name
