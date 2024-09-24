c7bcdab19f8c249a620a0b956778c48be61ad7e5
Victormuthomi <victormuhomi100@gmail.com>
2024-09-24 22:52:01 +0300

    Add the views for the workers app.

--- api/views.py
+++ api/views.py
@@ -4,10 +4,17 @@ from rest_framework import generics
 
 from books.models import Book
 
-from .serializers import BookSerializer
+from workers.models import Worker
+
+from .serializers import BookSerializer, WorkerSerializer
 
 # Create your views here.
-class BookAPIview(generics.ListAPIView):
+class BookAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class =BookSerializer
+
+class WorkerAPIView(generics.ListAPIView):
+        queryset = Worker.objects.all()
+        serializer_class = WorkerSerializer
+
 
